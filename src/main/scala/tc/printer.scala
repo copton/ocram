bb4b0479e19f70a88715ebc7d3df00c7773e4830
Alexander Bernauer <alex@copton.net>
2010-12-07 15:19:29 +0100

    project reset. Using CIL now.

--- src/main/scala/tc/printer.scala
+++
@@ -1,8 +0,0 @@
-package tc
-
-object Printer {
-	def apply(ast: xtc.tree.GNode, out: java.io.Writer) {
-		val printer = new xtc.lang.CPrinter(new xtc.tree.Printer(out))
-		printer.dispatch(ast)
-	}
-}
