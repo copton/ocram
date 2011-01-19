fad0cfec9f7ae9650d02ede3b4d4aebc935d92bd
Alexander Bernauer <alex@copton.net>
2011-01-19 19:01:43 +0100

    using current language.c version from darcs repo
    http://code.haskell.org/language-c

--- src/tc2ec.hs
+++ src/tc2ec.hs
@@ -10,7 +10,9 @@ main = do
     args <- getArgs
     when (length args < 1) usage
     let file = head args
-    parseMyFile file >>= printMyAst
+    ast <- parseMyFile file
+    printNodeInfo ast
+--    printMyAst ast
     where 
         usage = getProgName >>= \pn -> error $ "usage: " ++ pn ++ " <file>"
       
@@ -20,6 +22,14 @@ parseMyFile input_file =
      case parse_result of
        Left parse_err -> error (show parse_err)
        Right ast      -> return ast
+
+printNodeInfo :: CTranslUnit -> IO [()]
+printNodeInfo (CTranslUnit decls ni) = sequence $ pn ni : map pd decls
+    where pn (OnlyPos pos _) = putStrLn $ show pos
+          pn (NodeInfo pos _ name) = putStrLn $ show name ++ ": " ++ show pos
+          pd (CDeclExt (CDecl _ _ ni)) = putStrLn $ "CDeclExt: " ++ show ni
+          pd (CFDefExt (CFunDef _ _ _ _ ni)) = putStrLn $ "CFDefExt: " ++ show ni
+          pd (CAsmExt (CStrLit _ ni) _) = putStrLn $ "CAsmExt: " ++ show ni
 
 printMyAst :: CTranslUnit -> IO ()
 printMyAst ctu = (print . pretty) ctu
