96e434c2c986dc4d21ea424da7c9b81e8fa08c99
Alexander Bernauer <alex@copton.net>
2011-02-01 23:00:09 +0100

    still playing with the state monad

--- try/callgraph.hs
+++ try/callgraph.hs
@@ -14,20 +14,15 @@ data UserState = UserState {
     stmtCount :: Int
 } deriving Show
 
-funCb :: FunDef -> State UserState ()
---funCb fd = State $ \(UserState c c') -> ((), UserState (c+1) c')
--- funCb fd = do
---     UserState c c' <- get
---     put $ UserState (c+1) c'
---     return ()
-funCb fb = modify (\(UserState c c') -> UserState (c+1) c')
+funCb :: FunDef -> UserState -> UserState
+funCb fd (UserState c c') = UserState (c+1) c'
 
 travTranslUnit :: TranslUnit -> State UserState ()
 travTranslUnit (TranslUnit fds) = mapM_ travFunDef fds
 
 travFunDef :: FunDef -> State UserState ()
 travFunDef fd@(FunDef id stmt) = do
-    funCb fd
+    modify $ funCb fd
 --    travStmt stmt
     return ()
 
