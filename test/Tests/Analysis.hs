78b8d3414087c0f59756579f80401ff8760fe1a8
Alexander Bernauer <alex@copton.net>
2011-02-16 12:46:19 +0100

    test case for callgraph

--- test/Tests/Analysis.hs
+++ test/Tests/Analysis.hs
@@ -4,10 +4,13 @@ module Tests.Analysis (
 
 import qualified Tests.Analysis.StartRoutines
 import qualified Tests.Analysis.FunctionMap
+import qualified Tests.Analysis.CallGraph
+
 import Test.HUnit
 
 tests :: Test
 tests = TestLabel "Analysis" $ TestList [
             Tests.Analysis.StartRoutines.tests, 
-            Tests.Analysis.FunctionMap.tests
+            Tests.Analysis.FunctionMap.tests,
+            Tests.Analysis.CallGraph.tests
         ]
