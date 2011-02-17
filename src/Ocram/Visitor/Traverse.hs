b8c5d27253c0fe7587dc8f3e6855832a31fdd368
Alexander Bernauer <alex@copton.net>
2011-02-17 19:25:30 +0100

    wip

--- src/Ocram/Visitor/Traverse.hs
+++ src/Ocram/Visitor/Traverse.hs
@@ -1,161 +1,189 @@
 module Ocram.Visitor.Traverse (
-	execTrav,
 	traverseCTranslUnit
 ) where
 
-
 import Language.C.Syntax.AST
 import Language.C.Data.Ident (Ident)
-import Control.Monad.State
 import Ocram.Visitor.Visitor
 
-execTrav :: Visitor v => (a -> State v ()) -> a -> v -> v
-execTrav trav obj init = execState (trav obj) init
+trav :: (DownVisitor d, UpVisitor d u) => (a -> d -> u) -> a -> d -> [u]
+trav f x d = [f x d]
 
-maybeTraverse f a = maybe (return ()) f a
+maybeTrav :: (DownVisitor d, UpVisitor d u) => (a -> d -> u) -> Maybe a -> d -> [u]
+maybeTrav f (Just x) d = [f x d]
+maybeTrav f Nothing d = []
 
-traverseCTranslUnit :: Visitor v => CTranslUnit -> State v ()
-traverseCTranslUnit (CTranslUnit decls _) = mapM_ traverseCExtDecl decls
+mapTrav :: (DownVisitor d, UpVisitor d u) => (a -> d -> u) -> [a] -> d -> [u]
+mapTrav f xs d = map (\x -> f x d) xs
 
-traverseCExtDecl :: Visitor v => CExtDecl -> State v ()
-traverseCExtDecl ced@(CDeclExt cd) = modify (handleCExtDecl ced) >> traverseCDecl cd
-traverseCExtDecl ced@(CFDefExt cfd) = modify (handleCExtDecl ced) >> traverseCFunDef cfd
-traverseCExtDecl ced@(CAsmExt ca _) = modify (handleCExtDecl ced)
+traverseCTranslUnit :: (DownVisitor d, UpVisitor d u) => CTranslUnit -> d -> u
+traverseCTranslUnit ctu@(CTranslUnit decls _) d = u
+	where
+		d' = downCTranslUnit ctu d
+		us = mapTrav traverseCExtDecl decls d'
+		u  = upCTranslUnit ctu d' us
 
-traverseCDecl :: Visitor v => CDecl -> State v ()
+recurseCExtDecl :: (DownVisitor d, UpVisitor d u) => CExtDecl -> d -> (a -> d -> [u]) -> a -> u
+recurseCExtDecl ced d f x = u
+	where
+		d' = downCExtDecl ced d
+		us = f x d'
+		u  = upCExtDecl ced d' us
+
+traverseCExtDecl :: (DownVisitor d, UpVisitor d u) => CExtDecl -> d -> u
+traverseCExtDecl ced@(CDeclExt cd) d = u
+	where
+		d' = downCExtDecl ced d
+		us = trav traverseCDecl cd d'
+		u  = upCExtDecl ced d' us
+
+traverseCExtDecl ced@(CFDefExt cfd) d = u
+	where
+		d' = downCExtDecl ced d
+		us = trav traverseCFunDef cfd d'
+		u  = upCExtDecl ced d' us
+
+traverseCExtDecl ced@(CAsmExt _ _) d = u
+	where
+		d' = downCExtDecl ced d
+		u  = upCExtDecl ced d' []
+
+traverseCFunDef = undefined
+
+traverseCDecl :: (DownVisitor d, UpVisitor d u) => CDecl -> d -> u
 traverseCDecl cd@(CDecl _ decls _) = modify (handleCDecl cd) >> hDecls decls
 	where
 		hDecls [] = return ()
 		hDecls ((cd, ci, expr):ds) = 
-				  maybeTraverse traverseCDeclr cd
+					maybeTraverse traverseCDeclr cd
 			>> maybeTraverse traverseCInit ci
 			>> maybeTraverse traverseCExpr expr
 			>> hDecls ds
 
-traverseCFunDef :: Visitor v => CFunDef -> State v ()
-traverseCFunDef cfd@(CFunDef _ cdr cds cst _) = modify (handleCFunDef cfd)
-	>> traverseCDeclr cdr 
-	>> hCds cds 
-	>> traverseCStat cst
-	where
-		hCds [] = return ()
-		hCds (cd:cds) = traverseCDecl cd >> hCds cds
+-- traverseCFunDef :: (DownVisitor d, UpVisitor d u) => CFunDef -> d -> u
+-- traverseCFunDef cfd@(CFunDef _ cdr cds cst _) = modify (handleCFunDef cfd)
+--   >> traverseCDeclr cdr 
+--   >> hCds cds 
+--   >> traverseCStat cst
+--   where
+--     hCds [] = return ()
+--     hCds (cd:cds) = traverseCDecl cd >> hCds cds
 
-traverseCDeclr :: Visitor v => CDeclr -> State v ()
-traverseCDeclr cdr@(CDeclr id cdds _ _ _) = modify (handleCDeclr cdr)
-	>> maybeTraverse traverseIdent id 
-	>> hCdds cdds 
-	where
-		hCdds [] = return ()
-		hCdds (cdd:cdds) = modify (handleCDerivedDeclr cdd) >> hCdds cdds
+-- traverseCDeclr :: (DownVisitor d, UpVisitor d u) => CDeclr -> d -> u
+-- traverseCDeclr cdr@(CDeclr id cdds _ _ _) = modify (handleCDeclr cdr)
+--   >> maybeTraverse traverseIdent id 
+--   >> hCdds cdds 
+--   where
+--     hCdds [] = return ()
+--     hCdds (cdd:cdds) = modify (handleCDerivedDeclr cdd) >> hCdds cdds
 
-traverseCInit :: Visitor v => CInit -> State v ()
-traverseCInit ci@(CInitExpr ce _) = modify (handleCInit ci) >> traverseCExpr ce
-traverseCInit ci@(CInitList cis _) = modify (handleCInit ci) >> traverseCInitList cis
+-- traverseCInit :: (DownVisitor d, UpVisitor d u) => CInit -> d -> u
+-- traverseCInit ci@(CInitExpr ce _) = modify (handleCInit ci) >> traverseCExpr ce
+-- traverseCInit ci@(CInitList cis _) = modify (handleCInit ci) >> traverseCInitList cis
 
-traverseCInitList :: Visitor v => CInitList -> State v ()
-traverseCInitList cis = mapM_  (modify . handleCInit . snd) cis
+-- traverseCInitList :: (DownVisitor d, UpVisitor d u) => CInitList -> d -> u
+-- traverseCInitList cis = mapM_  (modify . handleCInit . snd) cis
 
-traverseIdent :: Visitor v => Ident -> State v ()
-traverseIdent id = modify $ handleIdent id
+-- traverseIdent :: (DownVisitor d, UpVisitor d u) => Ident -> d -> u
+-- traverseIdent id = modify $ handleIdent id
 
-traverseCBlockItem :: Visitor v => CBlockItem -> State v ()
-traverseCBlockItem (CBlockStmt cs1) = traverseCStat cs1
-traverseCBlockItem (CBlockDecl cd) = traverseCDecl cd
-traverseCBlockItem (CNestedFunDef cfd) = traverseCFunDef cfd
+-- traverseCBlockItem :: (DownVisitor d, UpVisitor d u) => CBlockItem -> d -> u
+-- traverseCBlockItem (CBlockStmt cs1) = traverseCStat cs1
+-- traverseCBlockItem (CBlockDecl cd) = traverseCDecl cd
+-- traverseCBlockItem (CNestedFunDef cfd) = traverseCFunDef cfd
 
-traverseCExpr :: Visitor v => CExpr -> State v ()
-traverseCExpr ce@(CComma ces _) = modify (handleCExpr ce) 
-	>> mapM_ traverseCExpr ces
-traverseCExpr ce@(CAssign _ ce1 ce2 _) = modify (handleCExpr ce) 
-	>> traverseCExpr ce1
-	>> traverseCExpr ce2
-traverseCExpr ce@(CCond ce1 ce2 ce3 _) = modify (handleCExpr ce)
-	>> traverseCExpr ce1
-	>> maybeTraverse traverseCExpr ce2
-	>> traverseCExpr ce3 
-traverseCExpr ce@(CBinary _ ce1 ce2 _) = modify (handleCExpr ce)
-	>> traverseCExpr ce1
-	>> traverseCExpr ce2
-traverseCExpr ce@(CCast cd ce1 _) = modify (handleCExpr ce)
-	>> traverseCDecl cd
-	>> traverseCExpr ce1
-traverseCExpr ce@(CUnary _ ce1 _) = modify (handleCExpr ce)
-	>> traverseCExpr ce1
-traverseCExpr ce@(CSizeofExpr ce1 _) = modify (handleCExpr ce)
-	>> traverseCExpr ce1
-traverseCExpr ce@(CSizeofType cd _) = modify (handleCExpr ce)
-	>> traverseCDecl cd
-traverseCExpr ce@(CAlignofExpr ce1 _) = modify (handleCExpr ce)
-	>> traverseCExpr ce1
-traverseCExpr ce@(CAlignofType cd _) = modify (handleCExpr ce)
-	>> traverseCDecl cd
-traverseCExpr ce@(CComplexReal ce1 _) = modify (handleCExpr ce)
-	>> traverseCExpr ce1
-traverseCExpr ce@(CComplexImag ce1 _) = modify (handleCExpr ce)
-	>> traverseCExpr ce1
-traverseCExpr ce@(CIndex ce1 ce2 _) = modify (handleCExpr ce)
-	>> traverseCExpr ce1
-	>> traverseCExpr ce2
-traverseCExpr ce@(CCall ce1 ces _) = modify (handleCExpr ce)
-	>> traverseCExpr ce1
-	>> mapM_ traverseCExpr ces
-traverseCExpr ce@(CMember ce1 id _ _) = modify (handleCExpr ce)
-	>> traverseCExpr ce1
-	>> traverseIdent id
-traverseCExpr ce@(CVar id _) = modify (handleCExpr ce)
-	>> traverseIdent id
-traverseCExpr ce@(CConst _) = modify (handleCExpr ce)
-traverseCExpr ce@(CCompoundLit cd cil _) = modify (handleCExpr ce)
-	>> traverseCDecl cd
-	>> traverseCInitList cil
-traverseCExpr ce@(CStatExpr cst _) = modify (handleCExpr ce)
-	>> traverseCStat cst
-traverseCExpr ce@(CLabAddrExpr id _) = modify (handleCExpr ce)
-	>> traverseIdent id
-traverseCExpr ce@(CBuiltinExpr _) = modify (handleCExpr ce)
+-- traverseCExpr :: (DownVisitor d, UpVisitor d u) => CExpr -> d -> u
+-- traverseCExpr ce@(CComma ces _) = modify (handleCExpr ce) 
+--   >> mapM_ traverseCExpr ces
+-- traverseCExpr ce@(CAssign _ ce1 ce2 _) = modify (handleCExpr ce) 
+--   >> traverseCExpr ce1
+--   >> traverseCExpr ce2
+-- traverseCExpr ce@(CCond ce1 ce2 ce3 _) = modify (handleCExpr ce)
+--   >> traverseCExpr ce1
+--   >> maybeTraverse traverseCExpr ce2
+--   >> traverseCExpr ce3 
+-- traverseCExpr ce@(CBinary _ ce1 ce2 _) = modify (handleCExpr ce)
+--   >> traverseCExpr ce1
+--   >> traverseCExpr ce2
+-- traverseCExpr ce@(CCast cd ce1 _) = modify (handleCExpr ce)
+--   >> traverseCDecl cd
+--   >> traverseCExpr ce1
+-- traverseCExpr ce@(CUnary _ ce1 _) = modify (handleCExpr ce)
+--   >> traverseCExpr ce1
+-- traverseCExpr ce@(CSizeofExpr ce1 _) = modify (handleCExpr ce)
+--   >> traverseCExpr ce1
+-- traverseCExpr ce@(CSizeofType cd _) = modify (handleCExpr ce)
+--   >> traverseCDecl cd
+-- traverseCExpr ce@(CAlignofExpr ce1 _) = modify (handleCExpr ce)
+--   >> traverseCExpr ce1
+-- traverseCExpr ce@(CAlignofType cd _) = modify (handleCExpr ce)
+--   >> traverseCDecl cd
+-- traverseCExpr ce@(CComplexReal ce1 _) = modify (handleCExpr ce)
+--   >> traverseCExpr ce1
+-- traverseCExpr ce@(CComplexImag ce1 _) = modify (handleCExpr ce)
+--   >> traverseCExpr ce1
+-- traverseCExpr ce@(CIndex ce1 ce2 _) = modify (handleCExpr ce)
+--   >> traverseCExpr ce1
+--   >> traverseCExpr ce2
+-- traverseCExpr ce@(CCall ce1 ces _) = modify (handleCExpr ce)
+--   >> traverseCExpr ce1
+--   >> mapM_ traverseCExpr ces
+-- traverseCExpr ce@(CMember ce1 id _ _) = modify (handleCExpr ce)
+--   >> traverseCExpr ce1
+--   >> traverseIdent id
+-- traverseCExpr ce@(CVar id _) = modify (handleCExpr ce)
+--   >> traverseIdent id
+-- traverseCExpr ce@(CConst _) = modify (handleCExpr ce)
+-- traverseCExpr ce@(CCompoundLit cd cil _) = modify (handleCExpr ce)
+--   >> traverseCDecl cd
+--   >> traverseCInitList cil
+-- traverseCExpr ce@(CStatExpr cst _) = modify (handleCExpr ce)
+--   >> traverseCStat cst
+-- traverseCExpr ce@(CLabAddrExpr id _) = modify (handleCExpr ce)
+--   >> traverseIdent id
+-- traverseCExpr ce@(CBuiltinExpr _) = modify (handleCExpr ce)
 
-traverseCStat :: Visitor v => CStat -> State v ()
-traverseCStat cs@(CLabel _ cs1 _ _) = modify (handleCStat cs)
-	>> traverseCStat cs1
-traverseCStat cs@(CCase ce1 cs1 _) = modify (handleCStat cs)
-	>> traverseCExpr ce1
-	>> traverseCStat cs1
-traverseCStat cs@(CCases ce1 ce2 cs1 _) = modify (handleCStat cs)
-	>> traverseCExpr ce1
-	>> traverseCExpr ce2
-	>> traverseCStat cs1
-traverseCStat cs@(CDefault cs1 _) = modify (handleCStat cs)
-	>> traverseCStat cs1
-traverseCStat cs@(CExpr ce1 _) = modify (handleCStat cs)
-	>> maybeTraverse traverseCExpr ce1 
-traverseCStat cs@(CCompound _ ccbis _) = modify (handleCStat cs)
-	>> mapM_ traverseCBlockItem ccbis
-traverseCStat cs@(CIf ce1 cs1 cs2 _) = modify (handleCStat cs)
-	>> traverseCExpr ce1
-	>> traverseCStat cs1
-	>> maybeTraverse traverseCStat cs2
-traverseCStat cs@(CSwitch ce1 cs1 _) = modify (handleCStat cs)
-	>> traverseCExpr ce1
-	>> traverseCStat cs1
-traverseCStat cs@(CWhile ce1 cs1 _ _) = modify (handleCStat cs)
-	>> traverseCExpr ce1
-	>> traverseCStat cs1
-traverseCStat cs@(CFor (Left ce1) ce2 ce3 cs1 _) = modify (handleCStat cs)
-	>> maybeTraverse traverseCExpr ce1
-	>> maybeTraverse traverseCExpr ce2
-	>> maybeTraverse traverseCExpr ce3
-	>> traverseCStat cs1
-traverseCStat cs@(CFor (Right cd1) ce1 ce2 cs1 _) = modify (handleCStat cs)
-	>> traverseCDecl cd1
-	>> maybeTraverse traverseCExpr ce1
-	>> maybeTraverse traverseCExpr ce2
-	>> traverseCStat cs1
-traverseCStat cs@(CGoto _ _) = modify (handleCStat cs)
-traverseCStat cs@(CGotoPtr ce1 _) = modify (handleCStat cs)
-	>> traverseCExpr ce1
-traverseCStat cs@(CCont _) = modify (handleCStat cs)
-traverseCStat cs@(CBreak _) = modify (handleCStat cs)
-traverseCStat cs@(CReturn ce1 _) = modify (handleCStat cs)
-	>> maybeTraverse traverseCExpr ce1
-traverseCStat cs@(CAsm _ _) = modify (handleCStat cs)
+-- traverseCStat :: (DownVisitor d, UpVisitor d u) => CStat -> d -> u
+-- traverseCStat cs@(CLabel _ cs1 _ _) = modify (handleCStat cs)
+--   >> traverseCStat cs1
+-- traverseCStat cs@(CCase ce1 cs1 _) = modify (handleCStat cs)
+--   >> traverseCExpr ce1
+--   >> traverseCStat cs1
+-- traverseCStat cs@(CCases ce1 ce2 cs1 _) = modify (handleCStat cs)
+--   >> traverseCExpr ce1
+--   >> traverseCExpr ce2
+--   >> traverseCStat cs1
+-- traverseCStat cs@(CDefault cs1 _) = modify (handleCStat cs)
+--   >> traverseCStat cs1
+-- traverseCStat cs@(CExpr ce1 _) = modify (handleCStat cs)
+--   >> maybeTraverse traverseCExpr ce1 
+-- traverseCStat cs@(CCompound _ ccbis _) = modify (handleCStat cs)
+--   >> mapM_ traverseCBlockItem ccbis
+-- traverseCStat cs@(CIf ce1 cs1 cs2 _) = modify (handleCStat cs)
+--   >> traverseCExpr ce1
+--   >> traverseCStat cs1
+--   >> maybeTraverse traverseCStat cs2
+-- traverseCStat cs@(CSwitch ce1 cs1 _) = modify (handleCStat cs)
+--   >> traverseCExpr ce1
+--   >> traverseCStat cs1
+-- traverseCStat cs@(CWhile ce1 cs1 _ _) = modify (handleCStat cs)
+--   >> traverseCExpr ce1
+--   >> traverseCStat cs1
+-- traverseCStat cs@(CFor (Left ce1) ce2 ce3 cs1 _) = modify (handleCStat cs)
+--   >> maybeTraverse traverseCExpr ce1
+--   >> maybeTraverse traverseCExpr ce2
+--   >> maybeTraverse traverseCExpr ce3
+--   >> traverseCStat cs1
+-- traverseCStat cs@(CFor (Right cd1) ce1 ce2 cs1 _) = modify (handleCStat cs)
+--   >> traverseCDecl cd1
+--   >> maybeTraverse traverseCExpr ce1
+--   >> maybeTraverse traverseCExpr ce2
+--   >> traverseCStat cs1
+-- traverseCStat cs@(CGoto _ _) = modify (handleCStat cs)
+-- traverseCStat cs@(CGotoPtr ce1 _) = modify (handleCStat cs)
+--   >> traverseCExpr ce1
+-- traverseCStat cs@(CCont _) = modify (handleCStat cs)
+-- traverseCStat cs@(CBreak _) = modify (handleCStat cs)
+-- traverseCStat cs@(CReturn ce1 _) = modify (handleCStat cs)
+--   >> maybeTraverse traverseCExpr ce1
+-- traverseCStat cs@(CAsm _ _) = modify (handleCStat cs)
--- src/Ocram/Visitor/Visitor.hs
+++ src/Ocram/Visitor/Visitor.hs
@@ -1,40 +1,84 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Ocram.Visitor.Visitor (
-	Visitor(..)
+	DownVisitor(..),
+	UpVisitor(..)
 ) where
 
 import Language.C.Syntax.AST
 import Language.C.Data.Ident
+import Data.Monoid
 
-class Visitor state where
-	handleCExtDecl :: CExtDecl -> state -> state
-	handleCExtDecl _  = id
+class DownVisitor downState where
+	downCTranslUnit :: CTranslUnit -> downState -> downState
+	downCTranslUnit _ = id
 
-	handleCFunDef :: CFunDef -> state -> state
-	handleCFunDef _ = id
+	downCExtDecl :: CExtDecl -> downState -> downState
+	downCExtDecl _  = id
 
-	handleCDecl :: CDecl -> state -> state
-	handleCDecl _ = id
+	downCFunDef :: CFunDef -> downState -> downState
+	downCFunDef _ = id
 
-	handleCStructUnion :: CStructUnion -> state -> state
-	handleCStructUnion _ = id
+	downCDecl :: CDecl -> downState -> downState
+	downCDecl _ = id
+
+	downCStructUnion :: CStructUnion -> downState -> downState
+	downCStructUnion _ = id
 	
-	handleCEnum :: CEnum -> state -> state
-	handleCEnum _ = id
+	downCEnum :: CEnum -> downState -> downState
+	downCEnum _ = id
 
-	handleCDeclr :: CDeclr -> state -> state
-	handleCDeclr _ = id
+	downCDeclr :: CDeclr -> downState -> downState
+	downCDeclr _ = id
 
-	handleCStat :: CStat -> state -> state
-	handleCStat _ = id
+	downCStat :: CStat -> downState -> downState
+	downCStat _ = id
 
-	handleCExpr :: CExpr -> state -> state
-	handleCExpr _ = id
+	downCExpr :: CExpr -> downState -> downState
+	downCExpr _ = id
 
-	handleIdent :: Ident -> state -> state
-	handleIdent _ = id
+	downIdent :: Ident -> downState -> downState
+	downIdent _ = id
 
-	handleCDerivedDeclr :: CDerivedDeclr -> state -> state
-	handleCDerivedDeclr _ = id
+	downCDerivedDeclr :: CDerivedDeclr -> downState -> downState
+	downCDerivedDeclr _ = id
 
-	handleCInit :: CInit -> state -> state
-	handleCInit _ = id
+	downCInit :: CInit -> downState -> downState
+	downCInit _ = id
+
+class (DownVisitor downState, Monoid upState) => UpVisitor downState upState where
+	upCTranslUnit :: CTranslUnit -> downState -> [upState] -> upState
+	upCTranslUnit _ _ = mconcat
+
+	upCExtDecl :: CExtDecl -> downState -> [upState] -> upState
+	upCExtDecl _ _ = mconcat
+
+	upCFunDef :: CFunDef -> downState -> [upState] -> upState
+	upCFunDef _ _ = mconcat
+
+	upCDecl :: CDecl -> downState -> [upState] -> upState
+	upCDecl _ _ = mconcat
+
+	upCStructUnion :: CStructUnion -> downState -> [upState] -> upState
+	upCStructUnion _ _ = mconcat
+	
+	upCEnum :: CEnum -> downState -> [upState] -> upState
+	upCEnum _ _ = mconcat
+
+	upCDeclr :: CDeclr -> downState -> [upState] -> upState
+	upCDeclr _ _ = mconcat
+
+	upCStat :: CStat -> downState -> [upState] -> upState
+	upCStat _ _ = mconcat
+
+	upCExpr :: CExpr -> downState -> [upState] -> upState
+	upCExpr _ _ = mconcat
+
+	upIdent :: Ident -> downState -> [upState] -> upState
+	upIdent _ _ = mconcat
+
+	upCDerivedDeclr :: CDerivedDeclr -> downState -> [upState] -> upState
+	upCDerivedDeclr _ _ = mconcat
+
+	upCInit :: CInit -> downState -> [upState] -> upState
+	upCInit _ _ = mconcat
