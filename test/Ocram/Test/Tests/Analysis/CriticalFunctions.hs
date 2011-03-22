20f59c7dd214fbe5f422c4317206660f4c73dfeb
Alexander Bernauer <alex@copton.net>
2011-03-22 19:12:19 +0100

    tests do not depend on the order any more

--- test/Ocram/Test/Tests/Analysis/CriticalFunctions.hs
+++ test/Ocram/Test/Tests/Analysis/CriticalFunctions.hs
@@ -7,15 +7,22 @@ import Ocram.Test.Tests.Analysis.Utils (runTests)
 import Ocram.Context (ctxCriticalFunctions)
 import Ocram.Analysis.Types.FunctionMap (funId)
 import Data.Set (elems)
+import Data.List (sort)
 
-reduce = (map funId).elems.ctxCriticalFunctions
+data L = L [String]
+instance Eq L where
+	(L ss) == (L ss') = (sort ss) == (sort ss')
+instance Show L where
+	show (L ss) = show ss
+
+reduce = L.(map funId).elems.ctxCriticalFunctions
 
 tests = runTests "CriticalFunctions" reduce [
-	 ("void foo() { }", [])
-	,("", [])
-	,("void foo();", [])
-	,("__attribute__((tc_blocking)) void foo();", ["foo"])
-	,("__attribute__((tc_blocking)) void foo(); void bar() { foo(); }", ["bar", "foo"])
-	,("__attribute__((tc_blocking)) void foo(); void bar(); void baz { bar(); foo(); }", ["baz", "foo"])
-	,("__attribute__((tc_blocking)) void D(); void B() {D();} void C() {D();} void A() {B();C();}", ["A","B","C","D"])
+	 ("void foo() { }", L [])
+	,("", L [])
+	,("void foo();", L [])
+	,("__attribute__((tc_blocking)) void foo();", L ["foo"])
+	,("__attribute__((tc_blocking)) void foo(); void bar() { foo(); }", L ["foo", "bar"])
+	,("__attribute__((tc_blocking)) void foo(); void bar(); void baz { bar(); foo(); }", L ["baz", "foo"])
+	,("__attribute__((tc_blocking)) void D(); void B() {D();} void C() {D();} void A() {B();C();}", L ["A","B","C","D"])
 	]
