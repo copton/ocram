4eac6d7b99669e4161fc1a2f02c37d8849f9fbbb
Alexander Bernauer <alex@copton.net>
2011-04-12 13:21:49 +0200

    removed dummy transformation module

--- src/Ocram/Transformation/Tc2ec.hs
+++
@@ -1,8 +0,0 @@
-module Ocram.Transformation.Tc2ec (tc2ec) where
-
-import Ocram.Types (Result, getAst, ValidAst, OutputAst(OutputAst))
-import Language.C.Syntax.AST (CTranslationUnit(CTranslUnit), CTranslUnit)
-import Language.C.Data.Node (undefNode)
-
-tc2ec :: ValidAst -> Result OutputAst
-tc2ec valid_ast = return $ OutputAst $ getAst valid_ast
