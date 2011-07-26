1e86bb17e8ee652f1af79354a8fbc7cbcb1664e9
Alexander Bernauer <alex@copton.net>
2011-07-26 13:38:04 +0200

    adapted make script to cabal infrastructure

--- test/Ocram/Test/Main.hs
+++ test/Ocram/Test/Main.hs
@@ -1,7 +1,14 @@
 module Ocram.Test.Main (main) where
 
 import Ocram.Test.Tests (tests)
-import Test.HUnit (runTestText, putTextToHandle)
+import Test.HUnit (runTestText, putTextToHandle, Counts(errors, failures))
 import System.IO (stderr)
+import System.Exit (exitWith, ExitCode(ExitFailure))
 
-main = runTestText (putTextToHandle stderr False) tests >> return ()
+main = do
+	(count, _ ) <- runTestText (putTextToHandle stderr False) tests
+	if errors count > 0 || failures count > 0
+		then
+			exitWith $ ExitFailure 1
+		else
+			return ()
