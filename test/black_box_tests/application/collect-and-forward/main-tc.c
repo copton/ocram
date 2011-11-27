6bb1c8ef363de26e8b85e594c29332653b3e6ff9
Alexander Bernauer <alex@copton.net>
2011-11-27 12:39:38 +0100

    alphabetical order of tasks

--- test/black_box_tests/application/collect-and-forward/main-tc.c
+++ test/black_box_tests/application/collect-and-forward/main-tc.c
@@ -8,9 +8,9 @@ int main()
 {
     tc_init();
 
-	tc_run_thread(&task_send);
-    tc_run_thread(&task_receive);
     tc_run_thread(&task_collect);
+    tc_run_thread(&task_receive);
+	tc_run_thread(&task_send);
 
     tc_run();
 
