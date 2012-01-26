4a2a321a680f852bd12c64430a4a85b59d9e68fb
Alexander Bernauer <alex@copton.net>
2012-01-26 12:43:25 +0100

    minor cleanup

--- src/Ocram/Analysis/CallGraph.hs
+++ src/Ocram/Analysis/CallGraph.hs
@@ -54,20 +54,6 @@ call_graph ast =
   in
     pruneGraph cg (Set.intersection cf rf)
 
-nodesByAttr :: GraphData -> (Attribute -> Bool) -> [G.Node]
-nodesByAttr g attr = map fst $ filter (any attr . lblAttr . snd) $ G.labNodes g
-
-reachableSubGraph :: GraphData -> [G.Node] -> Set.Set G.Node
-reachableSubGraph gd ns = Set.fromList $ concatMap (flip G.bfs gd) ns
-
-pruneGraph :: CallGraph -> Set.Set G.Node -> CallGraph
-pruneGraph (CallGraph gd gi) ns = 
-  let
-    gd' = filter_nodes (flip Set.member ns) gd
-    gi' = Map.filter (flip Set.member ns) gi
-  in
-    CallGraph gd' gi'
-
 from_test_graph :: [(Symbol, Symbol)] -> CallGraph -- {{{1
 from_test_graph edges =
   let
@@ -86,13 +72,6 @@ to_test_graph (CallGraph gd _) =
 
 blocking_functions :: CallGraph -> BlockingFunctions -- {{{1
 blocking_functions = functionsWith attrBlocking
-
-
-dependency_list :: CallGraph -> [Symbol] -- {{{1
-dependency_list cg@(CallGraph gd gi) = 
-  List.nub $ concatMap depList $ Set.toList $ start_functions cg
-  where
-    depList start = List.reverse $ map (gnode2symbol gd) $ G.bfs ($lookup_s gi start) gd
 
 start_functions :: CallGraph -> StartFunctions -- {{{1
 start_functions = functionsWith attrStart
@@ -118,6 +97,11 @@ call_order (CallGraph gd gi) start = do
   gstart <- Map.lookup start gi
   return $ map (gnode2symbol gd) $ List.nub $ G.bfs gstart gd
 
+dependency_list :: CallGraph -> [Symbol] -- {{{1
+dependency_list cg@(CallGraph gd gi) = 
+  List.nub $ concatMap depList $ Set.toList $ start_functions cg
+  where
+    depList start = List.reverse $ map (gnode2symbol gd) $ G.bfs ($lookup_s gi start) gd
 get_callees :: CallGraph -> Symbol -> Maybe [Symbol] -- {{{1
 get_callees (CallGraph gd gi) caller = do
   gcaller <- Map.lookup caller gi
@@ -126,17 +110,30 @@ get_callees (CallGraph gd gi) caller = do
 footprint :: CallGraph -> Footprint -- {{{1
 footprint cg@(CallGraph gd gi) = map footprint' (Set.toList $ start_functions cg)
   where
-  footprint' sf = filter isBlocking $ $fromJust_s $ call_order cg sf
-  isBlocking f = hasAttr attrBlocking $ $fromJust_s . G.lab gd $ $lookup_s gi f
+  footprint' sf = filter (is_blocking cg) $ $fromJust_s $ call_order cg sf
 
 -- utils {{{1
-gnode2symbol :: GraphData -> G.Node -> Symbol
+gnode2symbol :: GraphData -> G.Node -> Symbol -- {{{2
 gnode2symbol gd = lblName . $fromJust_s . G.lab gd
 
-createNodes :: [Label] -> [Node]
+nodesByAttr :: GraphData -> (Attribute -> Bool) -> [G.Node] -- {{{2
+nodesByAttr g attr = map fst $ filter (any attr . lblAttr . snd) $ G.labNodes g
+
+reachableSubGraph :: GraphData -> [G.Node] -> Set.Set G.Node -- {{{2
+reachableSubGraph gd ns = Set.fromList $ concatMap (flip G.bfs gd) ns
+
+pruneGraph :: CallGraph -> Set.Set G.Node -> CallGraph -- {{{2
+pruneGraph (CallGraph gd gi) ns = 
+  let
+    gd' = filter_nodes (flip Set.member ns) gd
+    gi' = Map.filter (flip Set.member ns) gi
+  in
+    CallGraph gd' gi'
+
+createNodes :: [Label] -> [Node] -- {{{2
 createNodes = zip [0..]
 
-createLabels :: Ast -> [Label]
+createLabels :: Ast -> [Label] -- {{{2
 createLabels (CTranslUnit ds _) = foldr go [] ds
   where
   go (CDeclExt x) labels
@@ -153,10 +150,10 @@ createLabels (CTranslUnit ds _) = foldr go [] ds
 
   go _ labels = labels
 
-createGraphIndex :: [Node] -> GraphIndex
+createGraphIndex :: [Node] -> GraphIndex -- {{{2
 createGraphIndex nodes = Map.fromList $ map (swap . second lblName) nodes
 
-createEdges :: Ast -> GraphIndex -> [Node] -> [Edge]
+createEdges :: Ast -> GraphIndex -> [Node] -> [Edge] -- {{{2
 createEdges ast gi nodes = foldl go [] nodes
   where
     go es (gcaller, Label caller _) =
@@ -172,23 +169,23 @@ createEdges ast gi nodes = foldl go [] nodes
     call (CCall (CVar (Ident callee _ _) _)  _ ni) = [(callee, ni)]
     call _ = []
 
-
-functionsWith :: (Attribute -> Bool) -> CallGraph -> Set.Set Symbol
+functionsWith :: (Attribute -> Bool) -> CallGraph -> Set.Set Symbol -- {{{2
 functionsWith pred cg = Set.fromList $ map lblName $ filter (hasAttr pred) $ map snd $ G.labNodes $ grData cg
 
-hasAttr :: (Attribute -> Bool) -> Label -> Bool
+hasAttr :: (Attribute -> Bool) -> Label -> Bool -- {{{2
 hasAttr pred (Label _ as) = any pred as
 
-attrBlocking :: Attribute -> Bool
+attrBlocking :: Attribute -> Bool -- {{{2
 attrBlocking Blocking = True
 attrBlocking _  = False
 
-attrStart :: Attribute -> Bool
+attrStart :: Attribute -> Bool -- {{{2
 attrStart Start = True
 attrStart _ = False
 
-functionIs :: (Attribute -> Bool) -> CallGraph -> Symbol -> Bool
+functionIs :: (Attribute -> Bool) -> CallGraph -> Symbol -> Bool -- {{{2
 functionIs pred (CallGraph gd gi) name =
   case Map.lookup name gi of
     Nothing -> False
     Just gnode -> any pred . lblAttr . $fromJust_s $ G.lab gd gnode
+
