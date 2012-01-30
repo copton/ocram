c3a8ce266c3e2794687de6cab48dd953618cf185
Alexander Bernauer <alex@copton.net>
2012-01-30 11:29:46 +0100

    Using Jinja for generation of simulation os PAL

--- applications/simulation_os/os/pal.c
+++ applications/simulation_os/os/pal.c
@@ -1,20 +1,7 @@
-#!/usr/bin/env python
-
-import sys
-
-if len(sys.argv) <= 1:
-    sys.stderr.write("There must be at least one thread.\n")
-    sys.exit(1)
-
-
-def headers(out):
-    out('#include "os/common.h"\n')
-
-def static_part(out):
-    out(r"""
+#include "os/common.h"
+/* pal_code */
 #include "os/core.h"
 #include "os/logger.h"
-
 #include <stdlib.h>
 
 typedef void (*thread_t)(void*);
@@ -54,6 +41,7 @@ typedef struct {
     frame->ec_result = result; \
     threads[ctid](frame->ec_cont);
 
+/*{ if "tc_sleep" in all_syscalls }*/
 static void sleep_cb(void* context, error_t result)
 {
     Context* ctx = (Context*) context;
@@ -66,7 +54,9 @@ void tc_sleep(ec_frame_tc_sleep_t * frame)
     NEW_CONTEXT;
 	os_sleep(&sleep_cb, ctx, frame->ms);
 }
+/*{ endif }*/
 
+/*{ if "tc_receive" in all_syscalls }*/
 static void receive_cb(void* context, error_t result, size_t len)
 {
     Context* ctx = (Context*) context;
@@ -80,7 +70,9 @@ void tc_receive(ec_frame_tc_receive_t * frame)
     NEW_CONTEXT;
 	os_receive(&receive_cb, ctx, frame->handle, frame->buffer, frame->buflen);
 }
+/*{ endif }*/
 
+/*{ if "tc_send" in all_syscalls }*/
 static void send_cb(void* context, error_t result)
 {
     Context* ctx = (Context*) context;
@@ -93,7 +85,9 @@ void tc_send(ec_frame_tc_send_t * frame)
     NEW_CONTEXT;
 	os_send(&send_cb, ctx, frame->handle, frame->buffer, frame->len);
 }
+/*{ endif }*/
 
+/*{ if "tc_flash_read" in all_syscalls }*/
 static void flash_read_cb(void* context, error_t result, size_t len)
 {
     Context* ctx = (Context*) context;
@@ -107,7 +101,9 @@ void tc_flash_read(ec_frame_tc_flash_read_t* frame)
     NEW_CONTEXT;
     os_flash_read(&flash_read_cb, ctx, frame->handle, frame->buffer, frame->buflen);
 }
+/*{ endif }*/
 
+/*{ if "tc_flash_write" in all_syscalls }*/
 static void flash_write_cb(void* context, error_t result)
 {
     Context* ctx = (Context*) context;
@@ -115,13 +111,14 @@ static void flash_write_cb(void* context, error_t result)
     RETURN_FROM_SYSCALL;
 }
 
-
 void tc_flash_write(ec_frame_tc_flash_write_t * frame)
 {
     NEW_CONTEXT;
 	os_flash_write(&flash_write_cb, ctx, frame->handle, frame->buffer, frame->len);
 }
+/*{ endif }*/
 
+/*{ if "tc_sensor_read" in all_syscalls }*/
 static void sensor_read_cb(void* context, error_t result, sensor_val_t value)
 {
     Context* ctx = (Context*) context;
@@ -135,24 +132,18 @@ void tc_sensor_read(ec_frame_tc_sensor_read_t * frame)
     NEW_CONTEXT;
 	os_sensor_read(&sensor_read_cb, ctx, frame->handle);
 }
-""")
+/*{ endif }*/
 
-def dynamic_part(out, numberof_threads):
-    out("\n".join(["void ec_thread_%d(void*);" % tid for tid in range(numberof_threads)]))
-    out("""
+/*{ for _ in thread_syscalls }*/
+void ec_thread_/*loop.index0*/(void*);
+/*{ endfor }*/
+
 int main()
 {
-    pal_init(%d);
-""" % numberof_threads)
-    out("\n".join(["pal_start_thread(&ec_thread_%d);" % tid for tid in range(numberof_threads)]))
-    out("""
+    pal_init(/*thread_syscalls|length*/);
+/*{ for _ in thread_syscalls }*/
+    pal_start_thread(&ec_thread_/*loop.index0*/);
+/*{ endfor }*/
     pal_run();
     return 0;
-}""")
-
-numberof_threads = len(sys.argv) - 1
-out = sys.stdout.write
-headers(out)
-out(sys.stdin.read())
-static_part(out)
-dynamic_part(out, numberof_threads)
+}
