806d0832aa117ee9c19e37f8b6a1e84ac3b45e1f
Alexander Bernauer <alex@copton.net>
2012-02-26 11:55:05 +0100

    basic setup

---
+++ applications/tinyos/blinky/generated/app-tc.c
@@ -0,0 +1,43 @@
+#ifdef OCRAM_MODE
+#define THREAD __attribute__((tc_run_thread))
+#define BLOCK __attribute__((tc_blocking))
+#else
+#define THREAD
+#define BLOCK
+#endif
+
+BLOCK void tc_sleep(uint32_t);
+uint32_t tc_time();
+void tc_toggle_led_0();
+void tc_toggle_led_1();
+void tc_toggle_led_2();
+
+THREAD void blink1()
+{
+    uint32_t now = tc_time();
+    while(1) {
+        now += 250;
+        tc_sleep(now);
+        tc_toggle_led_0();
+    }
+}
+
+THREAD void blink2()
+{
+    uint32_t now = tc_time();
+    while(1) {
+        now += 500;
+        tc_sleep(now);
+        tc_toggle_led_1();
+    }
+}
+
+THREAD void blink3()
+{
+    uint32_t now = tc_time();
+    while(1) {
+        now += 1000;
+        tc_sleep(now);
+        tc_toggle_led_2();
+    }
+}
