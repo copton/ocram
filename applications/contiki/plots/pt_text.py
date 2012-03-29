d2085fb4bf785e70dc6bc69142e6333a8ce91c37
Alexander Bernauer <alex@copton.net>
2012-03-29 16:38:42 +0200

    plotting PAL overhead to text (per thread)

--- applications/contiki/plots/pt_text.py
+++ applications/contiki/plots/pt_text.py
@@ -12,10 +12,10 @@ set palette gray
 set auto x
 set auto y
 
-plot '%(infile)s' using 2:xtic(1) title columnheader(2), '' using 3 title columnheader(3), '' using 4 title columnheader(4)
+plot '%(infile)s' using 2:xtic(1) title columnheader(2), '' using 3 title columnheader(3), '' using 4 title columnheader(4), '' using 5 title columnheader(5)
 """
 
-def plot(path, values, (apps_, variants_, measurements_)):
+def plot(path, values_, (apps_, variants_, measurements_)):
     measurements = ["text"]
     assert set(measurements).issubset(set(measurements_)), str(measurements_)
 
@@ -25,7 +25,14 @@ def plot(path, values, (apps_, variants_, measurements_)):
     variants = ["nat", "tl", "gen"]
     assert set(variants).issubset(set(variants_)), str(variants_)
 
-    plotdata = gnuplot.linespoints(values, apps, variants, ["text"])
+    values = {}
+    for a in apps:
+        for v in variants:
+            values[(a,v,"text")] = values_[(a,v,"text")]
+        values[(a,"gen_nopal", "text")] = values_[(a,"gen","text")] - values_[(a, "pal", "text")]
+
+    variants.append("gen_nopal")
+    plotdata = gnuplot.linespoints(values, apps, variants, measurements)
 
     config = {'outfile': os.path.join(path, "pt_text")}
     gnuplot.plot(script, config, plotdata)
