c2129f4e56f4fa6ccbee99f5fcd3d1c273011f16
Alexander Bernauer <alex@copton.net>
2012-03-30 22:01:07 +0200

    improved Contiki PAL
    no new benchmarks done, though.

--- applications/contiki/tc/pal.jinja.c
+++ applications/contiki/tc/pal.jinja.c
@@ -27,16 +27,10 @@ typedef enum {
 /*{ endfor }*/
 } Syscall;
 
-/*{ for syscall in all_syscalls }*/
-typedef struct {
-    ec_frame_/*syscall*/_t* frame;
-} ec_ctx_/*syscall*/_t;
-/*{ endfor }*/
- 
 typedef struct {
     union {
 /*{ for syscall in all_syscalls }*/
-        ec_ctx_/*syscall*/_t /*syscall*/;
+    ec_frame_/*syscall*/_t* /*syscall*/;
 /*{ endfor }*/
     } ctx;
     Syscall syscall;
@@ -46,7 +40,7 @@ ThreadContext threads[/*numberof_threads*/];
 
 /*{ for syscall in all_syscalls }*/
 void /*syscall*/(ec_frame_/*syscall*/_t* frame) {
-    threads[tid].ctx./*syscall*/.frame = frame;
+    threads[tid].ctx./*syscall*/ = frame;
     threads[tid].syscall = SYSCALL_/*syscall*/;
 }
 /*{ endfor }*/
@@ -79,72 +73,72 @@ PROCESS_THREAD(thread/*loop.index0*/, ev, data)
         if (0) { }
 /*{ if "tc_sleep" in all_syscalls }*/
         else if (threads[/*loop.index0*/].syscall == SYSCALL_tc_sleep) {
-            if (threads[/*loop.index0*/].ctx.tc_sleep.frame->cond) {
-                OBS("sleep: tics=%ld, cond.waiting=%d", threads[/*loop.index0*/].ctx.tc_sleep.frame->tics, threads[/*loop.index0*/].ctx.tc_sleep.frame->cond->waiting);
-                threads[/*loop.index0*/].ctx.tc_sleep.frame->cond->waiting = true;
-                threads[/*loop.index0*/].ctx.tc_sleep.frame->cond->waiting_process = PROCESS_CURRENT();
+            if (threads[/*loop.index0*/].ctx.tc_sleep->cond) {
+                OBS("sleep: tics=%ld, cond.waiting=%d", threads[/*loop.index0*/].ctx.tc_sleep->tics, threads[/*loop.index0*/].ctx.tc_sleep->cond->waiting);
+                threads[/*loop.index0*/].ctx.tc_sleep->cond->waiting = true;
+                threads[/*loop.index0*/].ctx.tc_sleep->cond->waiting_process = PROCESS_CURRENT();
             } else {
-                OBS("sleep: tics=%ld", threads[/*loop.index0*/].ctx.tc_sleep.frame->tics);
+                OBS("sleep: tics=%ld", threads[/*loop.index0*/].ctx.tc_sleep->tics);
             }
             static struct etimer et; 
             clock_time_t now = clock_time();
-            if (threads[/*loop.index0*/].ctx.tc_sleep.frame->tics > now) {
-                etimer_set(&et, threads[/*loop.index0*/].ctx.tc_sleep.frame->tics - now);
+            if (threads[/*loop.index0*/].ctx.tc_sleep->tics > now) {
+                etimer_set(&et, threads[/*loop.index0*/].ctx.tc_sleep->tics - now);
                 do {
                     PROCESS_YIELD();
                     if (ev == PROCESS_EVENT_TIMER && data == &et) {
-                        threads[/*loop.index0*/].ctx.tc_sleep.frame->ec_result = false;
+                        threads[/*loop.index0*/].ctx.tc_sleep->ec_result = false;
                         break;
                     }
-                    if (ev == PROCESS_EVENT_CONTINUE && data == &tc_condition_signal && threads[/*loop.index0*/].ctx.tc_sleep.frame->cond != NULL && threads[/*loop.index0*/].ctx.tc_sleep.frame->cond->waiting == false) {
-                        threads[/*loop.index0*/].ctx.tc_sleep.frame->ec_result = true;
+                    if (ev == PROCESS_EVENT_CONTINUE && data == &tc_condition_signal && threads[/*loop.index0*/].ctx.tc_sleep->cond != NULL && threads[/*loop.index0*/].ctx.tc_sleep->cond->waiting == false) {
+                        threads[/*loop.index0*/].ctx.tc_sleep->ec_result = true;
                         etimer_stop(&et);
                         break;
                     }
                 } while(1);
             } else {
                 PROCESS_PAUSE();
-                threads[/*loop.index0*/].ctx.tc_sleep.frame->ec_result = false;
+                threads[/*loop.index0*/].ctx.tc_sleep->ec_result = false;
             }
-            if (threads[/*loop.index0*/].ctx.tc_sleep.frame->cond) {
-                threads[/*loop.index0*/].ctx.tc_sleep.frame->cond->waiting = false;
+            if (threads[/*loop.index0*/].ctx.tc_sleep->cond) {
+                threads[/*loop.index0*/].ctx.tc_sleep->cond->waiting = false;
             }
-            continuation = threads[/*loop.index0*/].ctx.tc_sleep.frame->ec_cont;
+            continuation = threads[/*loop.index0*/].ctx.tc_sleep->ec_cont;
         }
 /*{ endif }*/ 
 /*{ if "tc_receive" in all_syscalls }*/
         else if (threads[/*loop.index0*/].syscall == SYSCALL_tc_receive) {
-            if (threads[/*loop.index0*/].ctx.tc_receive.frame->cond) {
-                OBS("receive: cond.waiting=%d", threads[/*loop.index0*/].ctx.tc_receive.frame->cond->waiting);
-                threads[/*loop.index0*/].ctx.tc_receive.frame->cond->waiting = true;
-                threads[/*loop.index0*/].ctx.tc_receive.frame->cond->waiting_process = PROCESS_CURRENT();
+            if (threads[/*loop.index0*/].ctx.tc_receive->cond) {
+                OBS("receive: cond.waiting=%d", threads[/*loop.index0*/].ctx.tc_receive->cond->waiting);
+                threads[/*loop.index0*/].ctx.tc_receive->cond->waiting = true;
+                threads[/*loop.index0*/].ctx.tc_receive->cond->waiting_process = PROCESS_CURRENT();
             } else {
                 OBS("receive: %s", "-");
             }
             do {
                 PROCESS_YIELD();
                 if (ev == tcpip_event && uip_newdata()) {
-                    threads[/*loop.index0*/].ctx.tc_receive.frame->ec_result = false;
+                    threads[/*loop.index0*/].ctx.tc_receive->ec_result = false;
                     break;
                 }
-                if (ev == PROCESS_EVENT_CONTINUE && data == &tc_condition_signal && threads[/*loop.index0*/].ctx.tc_sleep.frame->cond != NULL && threads[/*loop.index0*/].ctx.tc_sleep.frame->cond->waiting == false) {
-                    threads[/*loop.index0*/].ctx.tc_receive.frame->ec_result = true;
+                if (ev == PROCESS_EVENT_CONTINUE && data == &tc_condition_signal && threads[/*loop.index0*/].ctx.tc_sleep->cond != NULL && threads[/*loop.index0*/].ctx.tc_sleep->cond->waiting == false) {
+                    threads[/*loop.index0*/].ctx.tc_receive->ec_result = true;
                     break;
                 }
             } while(1);
-            if (threads[/*loop.index0*/].ctx.tc_receive.frame->cond) {
-                threads[/*loop.index0*/].ctx.tc_receive.frame->cond->waiting = false;
+            if (threads[/*loop.index0*/].ctx.tc_receive->cond) {
+                threads[/*loop.index0*/].ctx.tc_receive->cond->waiting = false;
             }
-            continuation = threads[/*loop.index0*/].ctx.tc_receive.frame->ec_cont;
+            continuation = threads[/*loop.index0*/].ctx.tc_receive->ec_cont;
         }
 /*{ endif }*/
 /*{ if "tc_condition_wait" in all_syscalls }*/
         else if (threads[/*loop.index0*/].syscall == SYSCALL_tc_condition_wait) {
-            OBS("condition_wait: cond.waiting=%d", threads[/*loop.index0*/].ctx.tc_condition_wait.frame->cond->waiting);
-            threads[/*loop.index0*/].ctx.tc_condition_wait.frame->cond->waiting_process = PROCESS_CURRENT();
-            threads[/*loop.index0*/].ctx.tc_condition_wait.frame->cond->waiting = true;
-            PROCESS_YIELD_UNTIL(ev == PROCESS_EVENT_CONTINUE && data == &tc_condition_signal && threads[/*loop.index0*/].ctx.tc_condition_wait.frame->cond->waiting == false);
-            continuation = threads[/*loop.index0*/].ctx.tc_condition_wait.frame->ec_cont;
+            OBS("condition_wait: cond.waiting=%d", threads[/*loop.index0*/].ctx.tc_condition_wait->cond->waiting);
+            threads[/*loop.index0*/].ctx.tc_condition_wait->cond->waiting_process = PROCESS_CURRENT();
+            threads[/*loop.index0*/].ctx.tc_condition_wait->cond->waiting = true;
+            PROCESS_YIELD_UNTIL(ev == PROCESS_EVENT_CONTINUE && data == &tc_condition_signal && threads[/*loop.index0*/].ctx.tc_condition_wait->cond->waiting == false);
+            continuation = threads[/*loop.index0*/].ctx.tc_condition_wait->ec_cont;
         }
 /*{ endif }*/
         else {
