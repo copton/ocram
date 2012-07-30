bc100a6a3a89eb2520cc332fdb6e20770d709de4
Alexander Bernauer <alex@copton.net>
2012-07-30 13:39:36 +0200

    cleanup

--- ruab/src/Ruab/Frontend.hs
+++ ruab/src/Ruab/Frontend.hs
@@ -23,15 +23,15 @@ import Ruab.Util (fromJust_s, abort)
 import qualified Ruab.Core as C
 import qualified Data.ByteString.Char8 as BS
 
--- types {{{1
-data Component = Component { -- {{{2
-    compCode  :: TextView
-  , compInfo  :: TextView
-  , compLines :: TextView
-  , compLabel :: Label
-  , compView  :: Viewport
-  }
+run :: Options -> IO () -- {{{1
+run opt = do
+  gui <- mfix (\gui' -> C.setup opt (statusUpdate gui') >>= loadGui)
+  setupGui gui
+  network <- compile $ createNetwork gui
+  actuate network
+  mainGUI
 
+-- types {{{1
 data GUI = GUI { -- {{{2
     guiWin     :: Window
   , guiTcomp   :: Component
@@ -44,38 +44,134 @@ data GUI = GUI { -- {{{2
   , guiCore    :: C.Context
   }
 
-run :: Options -> IO () -- {{{1
-run opt = do
-  gui <- mfix (\gui' -> C.setup opt (statusUpdate gui') >>= loadGui)
-  setupGui gui
-  network <- compile $ createNetwork gui
-  actuate network
-  mainGUI
+data Component = Component { -- {{{2
+    compCode  :: TextView
+  , compInfo  :: TextView
+  , compLines :: TextView
+  , compLabel :: Label
+  , compView  :: Viewport
+  }
 
-frontendStop :: C.Context -> IO ()  -- {{{2
-frontendStop core = do
-  _ <- C.shutdown core
-  mainQuit
+type InfoEvent = [InfoInstance] -> [InfoInstance] -- {{{2
 
--- GUI {{{1
-loadGui :: C.Context -> IO GUI -- {{{2
-loadGui core = do
-  gladefn <- getDataFileName "ruab.glade"
-  _ <- initGUI
-  Just xml <- xmlNew gladefn
-  window <- xmlGetWidget xml castToWindow "window"
-  [ct, cp, ce] <- mapM (loadComponent xml) ["t", "p", "e"]
-  [log', view] <- mapM (xmlGetWidget xml castToTextView) ["log", "view"]
-  input <- xmlGetWidget xml castToEntry "input"
-  status <- xmlGetWidget xml castToStatusbar "status"
-  return $ GUI window ct cp ce log' view input status core
+data Command -- {{{2
+  = CmdBreakAdd
+  | CmdBreakList
+  | CmdContinue
+  | CmdHelp
+  | CmdInterrupt
+  | CmdQuit
+  | CmdScroll
+  | CmdStart
+
+data CommandEvent -- {{{2
+  = CmdEvUnknown String
+  | CmdEvBreakAdd C.PRow
+  | CmdEvBreakList
+  | CmdEvContinue
+  | CmdEvHelp (Maybe Command)
+  | CmdEvInterrupt
+  | CmdEvQuit
+  | CmdEvScroll RowType Int
+  | CmdEvStart
+
+data RowType -- {{{2
+  = Tcode | Pcode | Ecode
+
+data LogEvent = LogEvent LogType [String] -- {{{2
+
+data LogType -- {{{2
+  = LogOutput
+  | LogError
+  | LogStatus
+  | LogPrompt
+
+-- semantics {{{1
+instance Read Command where -- {{{2
+  readsPrec _ command = case lookup command commands of
+    Nothing -> []
+    Just cmd -> [(cmd, "")]
+
+commands :: [(String, Command)] -- {{{2
+commands = [
+    ("badd", CmdBreakAdd)
+  , ("blist", CmdBreakList)
+  , ("continue", CmdContinue)
+  , ("help", CmdHelp)
+  , ("interrupt", CmdInterrupt)
+  , ("quit", CmdQuit)
+  , ("scroll", CmdScroll)
+  , ("start", CmdStart)
+  ]
+
+instance Read CommandEvent where -- {{{2
+  readsPrec _ cmdev = [(commandEvent cmdev, "")]
+
+commandEvent :: String -> CommandEvent -- {{{2
+commandEvent text =
+  let (command:options) = words text in
+  case mread command of
+    Nothing -> CmdEvUnknown command
+    Just cmd -> case cmd of
+      CmdBreakAdd -> case options of
+        [row@(mread -> Just (_ :: Int))] -> CmdEvBreakAdd $ (fromJust . mread) row
+        _ -> CmdEvHelp (Just CmdBreakAdd)
+
+      CmdBreakList -> noopt CmdBreakList CmdEvBreakList options
+      
+      CmdContinue -> noopt CmdContinue CmdEvContinue options
+
+      CmdHelp -> case options of
+        [cmd'] -> case mread cmd' of
+          Nothing -> CmdEvUnknown cmd'
+          Just cmd'' -> CmdEvHelp (Just cmd'')
+        _ -> CmdEvHelp Nothing
+
+      CmdInterrupt -> noopt CmdInterrupt CmdEvInterrupt options
+
+      CmdQuit -> noopt CmdQuit CmdEvQuit options
+
+      CmdScroll -> case options of
+        [rtype@((`elem`["t","p","e"]) -> True), row@(mread -> Just (_ :: Int))] ->
+          CmdEvScroll (read rtype)  ((fromJust . mread) row)
+        _ -> CmdEvHelp (Just CmdScroll)
+
+      CmdStart -> noopt CmdStart CmdEvStart options
+
   where
-    loadComponent xml comp = do
-      [code, info, lines] <- mapM (xmlGetWidget xml castToTextView) $ map (comp++) ["code", "info", "lines"]
-      label <- xmlGetWidget xml castToLabel (comp++"label")
-      view <- xmlGetWidget xml castToViewport (comp++"view")
-      return $ Component code info lines label view
+    noopt _   ev [] = ev
+    noopt cmd _  _  = CmdEvHelp (Just cmd)
 
+    mread x = listToMaybe [y | (y,"") <- reads x] 
+
+help :: Command -> [String] -- {{{2
+help CmdInterrupt = ["interrupt: interrupt execution"]
+help CmdContinue  = ["continue: continue execution"]
+help CmdBreakAdd  = ["badd prow: add a breakpoint"]
+help CmdBreakList = ["blist: list all breakpoints"]
+help CmdQuit = ["quit: quit ruab"]
+help CmdScroll = ["scroll t|p|e row: scroll views to the given row"]
+help CmdHelp = ["help: show the list of available commands"]
+help CmdStart = ["start: start execution"]
+
+instance Read RowType where -- {{{2
+  readsPrec _ rtype = case lookup rtype rowtypes of
+    Nothing -> []
+    Just rt -> [(rt, "")]
+    where
+      rowtypes = [
+          ("t", Tcode)
+        , ("p", Pcode)
+        , ("e", Ecode)
+        ]
+
+instance Show LogType where -- {{{2
+  show LogOutput = ">"
+  show LogError = "!"
+  show LogStatus = "@"
+  show LogPrompt = "$"
+
+-- event network {{{1
 createNetwork :: GUI -> NetworkDescription t () -- {{{2
 createNetwork gui = do
   eInput <- event_input (guiInput gui)
@@ -85,8 +181,8 @@ createNetwork gui = do
     eCommand = read <$> eInput
     infos = foldr (flip setBreakpoint 0) [] $ C.possible_breakpoints (guiCore gui)
     ePinfo = accumE infos eInfo
-    eTinfo = sync C.p2t_row <$> ePinfo
-    eEinfo = sync C.p2e_row <$> ePinfo
+    eTinfo = sync (C.p2t_row (guiCore gui)) <$> ePinfo
+    eEinfo = sync (C.p2e_row (guiCore gui)) <$> ePinfo
 
   -- echoing commands
   reactimate $ log (guiLog gui) . LogEvent LogPrompt . (:[]) <$> eInput
@@ -98,80 +194,18 @@ createNetwork gui = do
   reactimate $ update (guiEcomp gui) <$> eEinfo
   -- handle commands
   reactimate $ handleCommand (guiCore gui) fireLog fireInfo <$> eCommand
-    where
-      sync :: (C.Context -> C.PRow -> Maybe Int) -> [InfoInstance] -> [InfoInstance]
-      sync f infos = flip map infos $ \(prow, x) ->
-        case f (guiCore gui) prow of
-          Nothing -> $abort $ "failed to map row: " ++ show prow
-          Just row' -> (row', x)
 
-      update :: Component -> [InfoInstance] -> IO ()
-      update comp infos = postGUIAsync $ do
-        setText (compInfo comp) (render_info infos)
-        maybe (return ()) (scrollToRow comp . fst) $ find (infoIsHighlight . snd) infos
+sync :: (C.PRow -> Maybe Int) -> [InfoInstance] -> [InfoInstance] -- {{{2
+sync f infos = flip map infos $ \(prow, x) ->
+  case f prow of
+    Nothing -> $abort $ "failed to map row: " ++ show prow
+    Just row' -> (row', x)
 
-setupGui :: GUI -> IO () -- {{{2
-setupGui gui = do
-  setupComponent (guiTcomp gui)
-    ((C.t_code . guiCore) gui)
-    ("(T-code) " ++ (C.t_file . guiCore) gui)
+update :: Component -> [InfoInstance] -> IO () -- {{{2
+update comp infos = postGUIAsync $ do
+  setText (compInfo comp) (render_info infos)
+  maybe (return ()) (scrollToRow comp . fst) $ find (infoIsHighlight . snd) infos
 
-  setupComponent (guiPcomp gui)
-    ((C.p_code . guiCore) gui)
-    "(pre-processed)"
-
-  setupComponent (guiEcomp gui)
-    ((C.e_code . guiCore) gui)
-    ("(E-code) " ++ (C.e_file . guiCore) gui)
-
-  setupText (guiView gui) (BS.pack "")
-  setupText (guiLog gui) (BS.pack "")
-  addAppendMarker (guiLog gui)
-  _ <- onDestroy (guiWin gui) (frontendStop (guiCore gui))
-  widgetGrabFocus (guiInput gui)
-  widgetShowAll (guiWin gui)
-
-  where
-    addAppendMarker tv = do -- {{{3
-      buffer <- textViewGetBuffer tv
-      end <- textBufferGetEndIter buffer
-      _ <- textBufferCreateMark buffer (Just "append") end False 
-      return ()
-
-    setupComponent comp code caption = do -- {{{3
-      setupText (compCode comp) code
-      setupText (compInfo comp) (BS.pack "")
-      let rows = BS.lines code
-      let range = [1..(length rows)]
-      let text = BS.intercalate (BS.singleton '\n') $ map (BS.pack . show) range
-      setupText (compLines comp) text
-      labelSetText (compLabel comp) caption
-
-    setupText tv txt = do -- {{{3
-      buffer <- textBufferNew Nothing
-      textBufferInsertByteStringAtCursor buffer txt
-      textViewSetBuffer tv buffer
-
--- UI utils {{{1
-scrollToRow :: Component -> Int -> IO () -- {{{2
-scrollToRow comp row = do
-  buffer <- textViewGetBuffer (compCode comp)
-  maxRow <- textBufferGetLineCount buffer
-  adj <- viewportGetVAdjustment (compView comp)
-  upper <- adjustmentGetUpper adj
-  lower <- adjustmentGetLower adj
-  pageSize <- adjustmentGetPageSize adj
-  let ratio = fromIntegral (row - 1) / fromIntegral (maxRow - 1)
-  let range = (upper - pageSize) - lower
-  adjustmentSetValue adj $ ratio * range + lower
-  viewportSetVAdjustment (compView comp) adj
-
-setText :: TextView -> String -> IO () -- {{{2
-setText tv txt = do
-  buffer <- textViewGetBuffer tv
-  textBufferSetText buffer txt
-
--- actions {{{1
 log :: TextView -> LogEvent -> IO () -- {{{2
 log tv (LogEvent lt lines) = postGUIAsync $ do
   let
@@ -183,8 +217,6 @@ log tv (LogEvent lt lines) = postGUIAsync $ do
   mark <- textBufferGetMark buffer "append"
   textViewScrollToMark tv ($fromJust_s mark) 0 Nothing 
   return ()
-
-type FireEvent e = e -> IO ()
 
 handleCommand :: C.Context -> FireEvent LogEvent -> FireEvent InfoEvent -> CommandEvent -> IO () -- {{{2
 handleCommand core fireLog fireInfo event = handle event
@@ -236,124 +268,93 @@ handleCommand core fireLog fireInfo event = handle event
     lerror ss = fireLog $ LogEvent LogError ss
     lerror' s = lerror [s]
 
--- types {{{1
-type InfoEvent = [InfoInstance] -> [InfoInstance]
+type FireEvent e = e -> IO () -- {{{3
 
-commands :: [(String, Command)] -- {{{2
-commands = [
-    ("badd", CmdBreakAdd)
-  , ("blist", CmdBreakList)
-  , ("continue", CmdContinue)
-  , ("help", CmdHelp)
-  , ("interrupt", CmdInterrupt)
-  , ("quit", CmdQuit)
-  , ("scroll", CmdScroll)
-  , ("start", CmdStart)
-  ]
-  
-data Command -- {{{2
-  = CmdBreakAdd
-  | CmdBreakList
-  | CmdContinue
-  | CmdHelp
-  | CmdInterrupt
-  | CmdQuit
-  | CmdScroll
-  | CmdStart
+frontendStop :: C.Context -> IO ()  -- {{{2
+frontendStop core = do
+  _ <- C.shutdown core
+  mainQuit
 
-instance Read Command where -- {{{3
-  readsPrec _ command = case lookup command commands of
-    Nothing -> []
-    Just cmd -> [(cmd, "")]
+-- setup {{{1
+loadGui :: C.Context -> IO GUI -- {{{2
+loadGui core = do
+  gladefn <- getDataFileName "ruab.glade"
+  _ <- initGUI
+  Just xml <- xmlNew gladefn
+  window <- xmlGetWidget xml castToWindow "window"
+  [ct, cp, ce] <- mapM (loadComponent xml) ["t", "p", "e"]
+  [log', view] <- mapM (xmlGetWidget xml castToTextView) ["log", "view"]
+  input <- xmlGetWidget xml castToEntry "input"
+  status <- xmlGetWidget xml castToStatusbar "status"
+  return $ GUI window ct cp ce log' view input status core
+  where
+    loadComponent xml comp = do
+      [code, info, lines] <- mapM (xmlGetWidget xml castToTextView) $ map (comp++) ["code", "info", "lines"]
+      label <- xmlGetWidget xml castToLabel (comp++"label")
+      view <- xmlGetWidget xml castToViewport (comp++"view")
+      return $ Component code info lines label view
 
-data CommandEvent -- {{{2
-  = CmdEvUnknown String
-  | CmdEvBreakAdd C.PRow
-  | CmdEvBreakList
-  | CmdEvContinue
-  | CmdEvHelp (Maybe Command)
-  | CmdEvInterrupt
-  | CmdEvQuit
-  | CmdEvScroll RowType Int
-  | CmdEvStart
+setupGui :: GUI -> IO () -- {{{2
+setupGui gui = do
+  setupComponent (guiTcomp gui)
+    ((C.t_code . guiCore) gui)
+    ("(T-code) " ++ (C.t_file . guiCore) gui)
 
-instance Read CommandEvent where -- {{{3
-  readsPrec _ cmdev = [(commandEvent cmdev, "")]
+  setupComponent (guiPcomp gui)
+    ((C.p_code . guiCore) gui)
+    "(pre-processed)"
 
-data RowType -- {{{3
-  = Tcode | Pcode | Ecode
+  setupComponent (guiEcomp gui)
+    ((C.e_code . guiCore) gui)
+    ("(E-code) " ++ (C.e_file . guiCore) gui)
 
-instance Read RowType where -- {{{4
-  readsPrec _ rtype = case lookup rtype rowtypes of
-    Nothing -> []
-    Just rt -> [(rt, "")]
-    where
-      rowtypes = [
-          ("t", Tcode)
-        , ("p", Pcode)
-        , ("e", Ecode)
-        ]
-
-commandEvent :: String -> CommandEvent -- {{{2
-commandEvent text =
-  let (command:options) = words text in
-  case mread command of
-    Nothing -> CmdEvUnknown command
-    Just cmd -> case cmd of
-      CmdBreakAdd -> case options of
-        [row@(mread -> Just (_ :: Int))] -> CmdEvBreakAdd $ (fromJust . mread) row
-        _ -> CmdEvHelp (Just CmdBreakAdd)
-
-      CmdBreakList -> noopt CmdBreakList CmdEvBreakList options
-      
-      CmdContinue -> noopt CmdContinue CmdEvContinue options
-
-      CmdHelp -> case options of
-        [cmd'] -> case mread cmd' of
-          Nothing -> CmdEvUnknown cmd'
-          Just cmd'' -> CmdEvHelp (Just cmd'')
-        _ -> CmdEvHelp Nothing
-
-      CmdInterrupt -> noopt CmdInterrupt CmdEvInterrupt options
-
-      CmdQuit -> noopt CmdQuit CmdEvQuit options
-
-      CmdScroll -> case options of
-        [rtype@((`elem`["t","p","e"]) -> True), row@(mread -> Just (_ :: Int))] ->
-          CmdEvScroll (read rtype)  ((fromJust . mread) row)
-        _ -> CmdEvHelp (Just CmdScroll)
-
-      CmdStart -> noopt CmdStart CmdEvStart options
+  setupText (guiView gui) (BS.pack "")
+  setupText (guiLog gui) (BS.pack "")
+  addAppendMarker (guiLog gui)
+  _ <- onDestroy (guiWin gui) (frontendStop (guiCore gui))
+  widgetGrabFocus (guiInput gui)
+  widgetShowAll (guiWin gui)
 
   where
-    noopt _   ev [] = ev
-    noopt cmd _  _  = CmdEvHelp (Just cmd)
+    addAppendMarker tv = do -- {{{3
+      buffer <- textViewGetBuffer tv
+      end <- textBufferGetEndIter buffer
+      _ <- textBufferCreateMark buffer (Just "append") end False 
+      return ()
 
-    mread x = listToMaybe [y | (y,"") <- reads x] 
+    setupComponent comp code caption = do -- {{{3
+      setupText (compCode comp) code
+      setupText (compInfo comp) (BS.pack "")
+      let rows = BS.lines code
+      let range = [1..(length rows)]
+      let text = BS.intercalate (BS.singleton '\n') $ map (BS.pack . show) range
+      setupText (compLines comp) text
+      labelSetText (compLabel comp) caption
 
-help :: Command -> [String] -- {{{2
-help CmdInterrupt = ["interrupt: interrupt execution"]
-help CmdContinue  = ["continue: continue execution"]
-help CmdBreakAdd  = ["badd prow: add a breakpoint"]
-help CmdBreakList = ["blist: list all breakpoints"]
-help CmdQuit = ["quit: quit ruab"]
-help CmdScroll = ["scroll t|p|e row: scroll views to the given row"]
-help CmdHelp = ["help: show the list of available commands"]
-help CmdStart = ["start: start execution"]
+    setupText tv txt = do -- {{{3
+      buffer <- textBufferNew Nothing
+      textBufferInsertByteStringAtCursor buffer txt
+      textViewSetBuffer tv buffer
 
-data LogEvent = LogEvent LogType [String] -- {{{2
+-- utils {{{1
+scrollToRow :: Component -> Int -> IO () -- {{{2
+scrollToRow comp row = do
+  buffer <- textViewGetBuffer (compCode comp)
+  maxRow <- textBufferGetLineCount buffer
+  adj <- viewportGetVAdjustment (compView comp)
+  upper <- adjustmentGetUpper adj
+  lower <- adjustmentGetLower adj
+  pageSize <- adjustmentGetPageSize adj
+  let ratio = fromIntegral (row - 1) / fromIntegral (maxRow - 1)
+  let range = (upper - pageSize) - lower
+  adjustmentSetValue adj $ ratio * range + lower
+  viewportSetVAdjustment (compView comp) adj
 
-data LogType -- {{{2
-  = LogOutput
-  | LogError
-  | LogStatus
-  | LogPrompt
+setText :: TextView -> String -> IO () -- {{{2
+setText tv txt = do
+  buffer <- textViewGetBuffer tv
+  textBufferSetText buffer txt
 
-instance Show LogType where -- {{{2
-  show LogOutput = ">"
-  show LogError = "!"
-  show LogStatus = "@"
-  show LogPrompt = "$"
 
 -- old {{{1
 
