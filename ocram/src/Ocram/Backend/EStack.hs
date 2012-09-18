3164b82d2bc522588e2ab754b8595f174340294a
Alexander Bernauer <alex@copton.net>
2012-09-19 00:05:22 +0200

    test cases for create_estacks

--- ocram/src/Ocram/Backend/EStack.hs
+++ ocram/src/Ocram/Backend/EStack.hs
@@ -6,8 +6,9 @@ module Ocram.Backend.EStack
 ) where
 
 -- imports {{{1
+import Control.Monad ((<=<))
 import Data.Maybe (mapMaybe)
-import Ocram.Analysis (CallGraph, dependency_list, call_order)
+import Ocram.Analysis (CallGraph, dependency_list, call_order, start_functions)
 import Ocram.Intermediate (Function(..), fun_name, var_decl)
 import Language.C.Data.Ident (Ident, internalIdent)
 import Language.C.Data.Node (undefNode, NodeInfo)
@@ -19,20 +20,20 @@ import Ocram.Names (estackVar, eframe)
 import qualified Data.Map as M
 
 create_estacks :: CallGraph -> M.Map Symbol Function -> ([CDecl], M.Map Symbol (Maybe CDecl)) -- {{{1
-create_estacks cg funs = (frames, stacks)
+create_estacks cg cf = (frames, stacks)
   where
-    frames = mapMaybe (estackFrame . $fromJust_s . flip M.lookup funs) (dependency_list cg)
-    stacks = M.map (estackInstance cg funs) funs
+    frames = mapMaybe (estackFrame <=< flip M.lookup cf) (dependency_list cg)
+    stacks = M.fromList $ map (estackInstance cg cf) (start_functions cg)
 
-estackInstance :: CallGraph -> M.Map Symbol Function -> Function -> Maybe CDecl -- {{{2
-estackInstance cg funs fun
-  | null frames = Nothing
-  | otherwise   = Just stack
+estackInstance :: CallGraph -> M.Map Symbol Function -> Symbol -> (Symbol, Maybe CDecl) -- {{{2
+estackInstance cg cf fname
+  | null frames = (fname, Nothing)
+  | otherwise   = (fname, Just stack)
   where
     stack =
       CDecl [CTypeSpec (CSUType (CStruct CUnionTag Nothing (Just frames) [] un) un)] [(Just (CDeclr (Just (ii estackVar)) [] Nothing [] un), Nothing, Nothing)] un
 
-    frames = mapMaybe frame . mapMaybe (flip M.lookup funs) . $fromJust_s . call_order cg . fun_name $ fun
+    frames = mapMaybe frame . mapMaybe (flip M.lookup cf) . $fromJust_s . call_order cg $ fname
 
     frame callee
       | hasUncriticalVars callee = Just $
@@ -51,7 +52,7 @@ estackFrame fun
     uncriticalVariables = map var_decl (fun_ncVars fun)
 
 hasUncriticalVars :: Function -> Bool
-hasUncriticalVars = null . fun_ncVars
+hasUncriticalVars = not . null . fun_ncVars
 
 un :: NodeInfo
 un = undefNode
--- ocram/src/Ocram/Backend/Test.hs
+++ ocram/src/Ocram/Backend/Test.hs
@@ -4,6 +4,7 @@ module Ocram.Backend.Test (tests) where
 -- imports {{{1
 import Language.C.Data.Node (undefNode)
 import Language.C.Syntax.AST
+import Language.C.Pretty (pretty)
 import Ocram.Analysis (analysis, Analysis(..))
 import Ocram.Backend.BlockingFunctionDeclaration
 import Ocram.Backend.EStack
@@ -13,10 +14,15 @@ import Ocram.Intermediate (ast_2_ir)
 import Ocram.Test.Lib (enumTestGroup, enrich, reduce, paste)
 import Ocram.Text (show_errors)
 import Test.Framework (Test, testGroup)
-import Test.HUnit (Assertion, (@=?))
+import Test.HUnit (Assertion, (@=?), assertEqual)
+
+import qualified Data.Map as M
 
 tests :: Test -- {{{1
-tests = testGroup "Backend" [test_create_tstacks]
+tests = testGroup "Backend" [
+    test_create_tstacks
+  , test_create_estacks
+  ]
 
 test_create_tstacks :: Test  -- {{{1
 test_create_tstacks = enumTestGroup "create_tstacks" $ map runTest [
@@ -176,3 +182,118 @@ test_create_tstacks = enumTestGroup "create_tstacks" $ map runTest [
         expectedDecls' = (reduce (enrich expectedDecls :: CTranslUnit)) :: String
       in
         expectedDecls' @=? outputDecls
+
+test_create_estacks :: Test -- {{{1
+test_create_estacks = enumTestGroup "create_estacks" $ map runTest [
+  -- , 01 -  minimal case {{{2
+  ([paste|
+    __attribute__((tc_blocking)) void block();
+    __attribute__((tc_run_thread)) void start() {
+      block();
+    }
+  |], "", [("start", Nothing)])
+  , -- 02 - uncritical variable {{{2
+  ([paste|
+    __attribute__((tc_blocking)) void block();
+    __attribute__((tc_run_thread)) void start() {
+      int i = 23;
+      block(i);
+    }
+  |], [paste|
+    typedef struct { 
+      int i;
+    } ec_eframe_start_t;
+  |], [
+      ("start", Just "union {\n    ec_eframe_start_t start;\n} ec_estack")
+  ])
+  , -- 03 - critical function {{{2
+  ([paste| 
+    __attribute__((tc_blocking)) void block(int i);
+    int crit() { int k = 42; block(k);}
+    __attribute__((tc_run_thread)) void start() {
+      int i = 23;
+      crit(i);
+    }
+  |], [paste|
+    typedef struct {
+      int k;
+    } ec_eframe_crit_t;
+
+    typedef struct {
+      int i;
+    } ec_eframe_start_t; 
+  |], [
+      ("start", Just "union {\n    ec_eframe_start_t start; ec_eframe_crit_t crit;\n} ec_estack")
+  ]) 
+  , -- 04 - two threads {{{2
+  ([paste|
+    __attribute__((tc_blocking)) void block(int i);
+    __attribute__((tc_run_thread)) void start() {
+      int i = 23;
+      block(i);
+    }
+    __attribute__((tc_run_thread)) void run() {
+      int j = 42;
+      block(j);
+    }
+  |], [paste|
+    typedef struct {
+      int i;
+    } ec_eframe_start_t;
+
+    typedef struct {
+      int j;
+    } ec_eframe_run_t;
+  |], [
+      ("run"  , Just "union {\n    ec_eframe_run_t run;\n} ec_estack")
+    , ("start", Just "union {\n    ec_eframe_start_t start;\n} ec_estack")
+  ])
+  , -- 04 - reentrance {{{2
+  ([paste|
+    __attribute__((tc_blocking)) void block(int i);
+    int crit() { int k = 23; block(k); }
+    __attribute__((tc_run_thread)) void start() {
+      int i = 42;
+      crit(i);
+    }
+    __attribute__((tc_run_thread)) void run() {
+      int j = 0xdeadbeef;
+      crit(j);
+    }
+  |], [paste|
+    typedef struct {
+      int k; 
+    } ec_eframe_crit_t;
+
+    typedef struct {
+      int i;
+    } ec_eframe_start_t;
+
+    typedef struct {
+      int j;
+    } ec_eframe_run_t;
+  |], [
+      ("run"  , Just "union {\n    ec_eframe_run_t run; ec_eframe_crit_t crit;\n} ec_estack")
+    , ("start", Just "union {\n    ec_eframe_start_t start; ec_eframe_crit_t crit;\n} ec_estack")
+  ])
+    
+  -- end {{{2
+  ]
+  where
+    runTest :: (String, String, [(String, Maybe String)]) -> Assertion -- {{{2
+    runTest (inputCode, expectedFrames, expectedStacks) =
+      let
+        ast = enrich inputCode :: CTranslUnit
+        ana = case analysis ast of
+          Left es -> error $ show_errors "test" es 
+          Right x -> x
+        ir = ast_2_ir (anaCritical ana)
+        (frames, stacks) = create_estacks (anaCallgraph ana) ir        
+        expectedFrames' = reduce (enrich expectedFrames :: CTranslUnit) :: String
+        outputFrames = reduce (CTranslUnit (map CDeclExt frames) undefNode) :: String
+        outputStacks = M.toList . M.map (fmap (show . pretty)) $ stacks 
+      in do
+        assertEqual "frames" expectedFrames' outputFrames
+        assertEqual "stacks" expectedStacks outputStacks
+        
+
