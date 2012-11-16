d004dfce26622c9ed087764f72bfaf47d04eee4a
Alexander Bernauer <alex@copton.net>
2012-11-16 16:31:16 +0100

    minor correction for allTVars

--- ocram/src/Ocram/Backend/Utils.hs
+++ ocram/src/Ocram/Backend/Utils.hs
@@ -8,7 +8,7 @@ allEVars :: Function -> [FunctionVariable] -- {{{1
 allEVars = filter (not . or . sequence [fvar_critical, fvar_parameter, fvar_static]) . fun_vars
 
 allTVars :: Function -> [FunctionVariable] -- {{{1
-allTVars = filter ((&&) <$> not . fvar_static <*> or . sequence [fvar_critical, fvar_parameter]) . fun_vars
+allTVars = filter (or . sequence [(&&) <$> fvar_critical <*> not . fvar_static, fvar_parameter]) . fun_vars
 
 allSVars :: Function -> [FunctionVariable] -- {{{1
 allSVars = filter fvar_static . fun_vars
